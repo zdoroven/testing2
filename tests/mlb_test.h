a2325e282d49ff7619a3e25252ace3720f09d0f3
egorhristoforov <egor.hristoforov@mail.ru>
2019-04-23 12:04:51 +0300

    Add new test for mlb

--- tests/mlb_test.h
+++ tests/mlb_test.h
@@ -2,7 +2,7 @@
 #define MLB_TEST_H
 
 #include <gtest/gtest.h>
-#include <sys/io.h>
+#include <sys/uio.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -146,6 +146,57 @@ TEST(mlb_test, test2) {
     TRAVIS ? expectedData = fopen("tests/expected/expected10.txt", "r") : expectedData = fopen("../../testing2/tests/expected/expected10.txt", "r");
     FILE *outputData;
     TRAVIS ? outputData = fopen("tests/output/output10.txt", "r") : outputData = fopen("../../testing2/tests/output/output10.txt", "r");
+
+    if (executeTest(expectedData, outputData) == 1) {
+        SUCCEED();
+    } else {
+        FAIL();
+    }
+}
+
+TEST(mlb_test, test3) {
+    /*
+        Open output file
+    */
+    FILE *outputFile;
+    TRAVIS ? outputFile = fopen("tests/output/output11.txt", "wb") : outputFile = fopen("../../testing2/tests/output/output11.txt", "wb");
+    if (outputFile == NULL) {
+        printf("Cannot open file for output");
+        FAIL();
+    }
+    int oldstdOut = changeStream(outputFile);
+
+    /*
+        Load input data
+    */
+
+    text txt = create_text();
+    char inFile[MAXLINE];
+    TRAVIS ? strncpy(inFile, "tests/input/input11.txt", MAXLINE) : strncpy(inFile, "../../testing2/tests/input/input11.txt", MAXLINE);
+    load(txt, inFile);
+
+    /*
+        Run test function
+    */
+
+   //mwcrsr(txt, 3, 3);
+   cursor_to_begin(txt);
+   show(txt);
+
+    /*
+        Close output file
+    */
+
+    returnStream(outputFile, oldstdOut);
+
+    /*
+        Execute test
+    */
+
+    FILE *expectedData;
+    TRAVIS ? expectedData = fopen("tests/expected/expected11.txt", "r") : expectedData = fopen("../../testing2/tests/expected/expected11.txt", "r");
+    FILE *outputData;
+    TRAVIS ? outputData = fopen("tests/output/output11.txt", "r") : outputData = fopen("../../testing2/tests/output/output11.txt", "r");
 
     if (executeTest(expectedData, outputData) == 1) {
         SUCCEED();
